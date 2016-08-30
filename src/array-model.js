2283a7bd526830be660d59eedc213677e26ea53a
sylvainpolletvillard <sylvain.pollet.villard@gmail.com>
2016-08-30 22:34:34 +0200

    fixed https://github.com/sylvainpolletvillard/ObjectModel/issues/26

--- src/array-model.js
+++ src/array-model.js
@@ -7,10 +7,18 @@ Model[ARRAY] = function ArrayModel(def){
 		if(isProxySupported){
 			proxy = new Proxy(array, {
 				get: function (arr, key) {
-					return (ARRAY_MUTATOR_METHODS.indexOf(key) >= 0 ? proxifyArrayMethod(arr, key, model) : arr[key]);
+					if(key === CONSTRUCTOR){
+						return model;
+					} else if(ARRAY_MUTATOR_METHODS.indexOf(key) >= 0){
+						return proxifyArrayMethod(arr, key, model);
+					}
+					return arr[key];
 				},
 				set: function (arr, key, val) {
 					setArrayKey(arr, key, val, model);
+				},
+				getPrototypeOf: function(){
+					return model[PROTO];
 				}
 			});
 		} else {
@@ -24,9 +32,9 @@ Model[ARRAY] = function ArrayModel(def){
 			ARRAY_MUTATOR_METHODS.forEach(function (method) {
 				define(proxy, method, proxifyArrayMethod(array, method, model, proxy));
 			});
+			setConstructor(proxy, model);
 		}
 
-		setConstructor(proxy, model);
 		return proxy;
 	};
 

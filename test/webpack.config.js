8b046de96da64a4ab2ad215f09848be4bb24595a
sylvainpolletvillard <sylvain.pollet.villard@gmail.com>
2017-05-16 21:52:17 +0200

    added sourcemaps to test bundle

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -2,6 +2,7 @@ const glob = require("glob");
 const path = require("path");
 
 module.exports = {
+	devtool: "cheap-module-eval-source-map",
 	entry: glob.sync("./test/*.spec.js"),
 	output: {
 		path: path.resolve(__dirname, "dist"),

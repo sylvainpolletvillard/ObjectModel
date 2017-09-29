d3cf4f00cc10c2965ab6a74d9cb6cd4efe4a9f8f
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2017-09-30 00:21:33 +0200

    updated deps

--- dist/object-model.js
+++ dist/object-model.js
@@ -1,5 +1,5 @@
 /*!
- * Objectmodel v3.1.0
+ * Objectmodel v3.1.1
  * http://objectmodel.js.org
  *
  * Copyright (c) 2017 Sylvain Pollet-Villard
@@ -9,7 +9,7 @@
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 	typeof define === 'function' && define.amd ? define(['exports'], factory) :
-	(factory((global.window = global.window || {})));
+	(factory((global.window = {})));
 }(this, (function (exports) { 'use strict';
 
 const _constructor = "_constructor";
--- rollup.config.js
+++ rollup.config.js
@@ -3,11 +3,13 @@ import uglify from "rollup-plugin-uglify-es"
 const isProduction = process.env.BUILD === 'production';
 
 export default {
-	entry: 'build/bundle-entry' + (isProduction ? '.js' : '.dev.js'),
-	dest: 'dist/object-model' + (isProduction ? '.min.js' : '.js'),
-	format: 'umd',
-	moduleName: 'window',
-	sourceMap: true,
+	input: 'build/bundle-entry' + (isProduction ? '.js' : '.dev.js'),
+	output: {
+		file: 'dist/object-model' + (isProduction ? '.min.js' : '.js'),
+		format: 'umd'
+	},
+	name: 'window',
+	sourcemap: true,
 	plugins: isProduction ? [uglify({
 		compress: {
 			evaluate: false //prevent an unsafe compression

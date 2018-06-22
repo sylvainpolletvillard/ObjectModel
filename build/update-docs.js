69ed4c5631fcdaf5c373cdeac600abb2742a374c
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2018-06-22 02:14:22 +0200

    remove non-gzipped size in docs

--- build/update-docs.js
+++ build/update-docs.js
@@ -6,13 +6,11 @@ const pkg = require('../package.json');
 
 function updateIndex(html){
 	const libPath = "./dist/object-model.min.js"
-	const minSize = fs.statSync(libPath).size;
 	const gzipSize = getGzipSize(fs.readFileSync(libPath, 'utf8'));
 
-	console.info(`${pkg.name} v${pkg.version}: ${filesize(minSize)} minified, ${filesize(gzipSize)} gzipped`)
+	console.info(`${pkg.name} v${pkg.version}: ${filesize(gzipSize)} minified and gzipped`)
 
 	return html
-		.replace(/(<strong class="size-min">)([^<]+)(<\/strong>)/g, `$1${ filesize(minSize) }$3`)
 		.replace(/(<strong class="size-gzip">)([^<]+)(<\/strong>)/g, `$1${ filesize(gzipSize) }$3`)
 		.replace(/(<span class="version">)([^<]+)(<\/span>)/g, `$1${ pkg.version }$3`)
 		.replace(/(<a class="link-zip")([^<]+)(<\/a>)/g,

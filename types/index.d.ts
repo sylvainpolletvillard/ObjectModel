38edd0d466a0b70e6d85f234b77364935400da12
sylvainpolletvillard <sylvain.pollet.villard@gmail.com>
2018-08-19 00:07:16 +0200

    improvements to typescript definitions

--- types/index.d.ts
+++ types/index.d.ts
@@ -8,58 +8,58 @@ export interface Model {
 	default: any;
 	name: string;
 
-	as(name: string): this;
+	conventionForConstant(variableName: string): boolean;
+	conventionForPrivate(variableName: string): boolean;
 
 	toString(stack?: any[]): string;
 
-	errorCollector(errors: ModelError[]);
-
-	extend(...otherDefinitions: Array<any>): this;
-
-	assert(assertion: Assertion, description?: string | Function): this;
+	as(name: string): this;
 
 	defaultTo(defaultValue: any): this;
 
-	test(value: any): boolean;
-
 	validate(instance: any, errorCollector?: (errors: ModelError[]) => void): void;
 
-	conventionForConstant(variableName: string): boolean;
+	test(value: any): boolean;
 
-	conventionForPrivate(variableName: string): boolean;
+	errorCollector(errors: ModelError[]);
+
+	assert(assertion: Assertion, description?: string | Function): this;
+
 }
 
 export interface ModelConstructor {
 	(definition: any, params?: ObjectModelParams): ObjectModel;
-	new (definition: any, params?: ObjectModelParams): ObjectModel;
+	new(definition: any, params?: ObjectModelParams): ObjectModel;
 }
 
 export interface BasicModel extends Model {
 	(): any;
-	new (): any;
+	new(): any;
 	(value: any): any
-	new (value: any): any
+	new(value: any): any
+
+	extend(...otherDefinitions: Array<any>): this;
 }
 
 export interface BasicModelConstructor {
 	(definition: any): BasicModel
-	new (definition: any): BasicModel;
+	new(definition: any): BasicModel;
 }
 
 export interface ObjectModel extends Model {
-	(): any;
-	new (): any;
-	(object: object): any;
-	new (object: object): any;
-
-	defaults(defaultValuesObject: object);
+	(): Object;
+	new(): Object;
+	(object: object): Object;
+	new(object: object): Object;
 
 	sealed: boolean;
+
+	extend(...otherDefinitions: Array<Object | ObjectModel>): this;
 }
 
 export interface ObjectModelConstructor {
 	(definition: Object, params?: ObjectModelParams): ObjectModel;
-	new (definition: Object, params?: ObjectModelParams): ObjectModel;
+	new(definition: Object, params?: ObjectModelParams): ObjectModel;
 }
 
 export interface ObjectModelParams {
@@ -71,51 +71,59 @@ export interface ArrayModel extends Model {
 	new <T>(): Array<T>;
 	<T>(array: Array<T>): Array<T>;
 	new <T>(array: Array<T>): Array<T>;
+
+	extend(...otherElementDefinitions: Array<any>): this;
 }
 
 export interface ArrayModelConstructor {
 	(itemDefinition: any): ArrayModel;
-	new (itemDefinition: any): ArrayModel;
+	new(itemDefinition: any): ArrayModel;
 }
 
 export interface FunctionModel extends Model {
 	(): Function;
-	new (): Function;
+	new(): Function;
 	(fn: Function): Function;
-	new (fn: Function): Function;
+	new(fn: Function): Function;
 
 	definition: { arguments: any[], return: any };
 
 	return(returnValueDefinition: any): FunctionModel;
+
+	extend(otherArgsDefinitions: Array<any>, otherReturnValuesDefinitions: Array<any>): this;
 }
 
 export interface FunctionModelConstructor {
 	(...argumentsDefinitions: any[]): FunctionModel;
-	new (...argumentsDefinitions: any[]): FunctionModel;
+	new(...argumentsDefinitions: any[]): FunctionModel;
 }
 
 export interface MapModel extends Model {
-	(): Map<any,any>;
-	new (): Map<any,any>;
-	(iterable: Map<any,any> | Array<[any, any]>): Map<any,any>;
-	new (iterable: Map<any,any> | Array<[any, any]>): Map<any,any>;
+	(): Map<any, any>;
+	new(): Map<any, any>;
+	(iterable: Map<any, any> | Array<[any, any]>): Map<any, any>;
+	new(iterable: Map<any, any> | Array<[any, any]>): Map<any, any>;
+
+	extend(otherKeyDefinitions: Array<any>, otherValueDefinitions: Array<any>): this;
 }
 
 export interface MapModelConstructor {
 	(keyDefinition: any, valueDefinition: any): MapModel;
-	new (keyDefinition: any, valueDefinition: any): MapModel;
+	new(keyDefinition: any, valueDefinition: any): MapModel;
 }
 
 export interface SetModel extends Model {
 	(): Set<any>;
-	new (): Set<any>;
+	new(): Set<any>;
 	(set: Set<any> | Array<any>): Set<any>;
-	new (set: Set<any> | Array<any>): Set<any>;
+	new(set: Set<any> | Array<any>): Set<any>;
+
+	extend(...otherElementDefinitions: Array<any>): this;
 }
 
 export interface SetModelConstructor {
 	(itemDefinition: any): SetModel;
-	new (itemDefinition: any): SetModel;
+	new(itemDefinition: any): SetModel;
 }
 
 export type Assertion = (variable: any) => boolean

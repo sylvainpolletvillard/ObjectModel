014eb52206112c81d8b3a79a7630f6cb8d5174c0
sylvainpolletvillard <sylvain.pollet.villard@gmail.com>
2018-08-21 01:21:09 +0200

    perf: add cast mode to list models

--- src/list-model.js
+++ src/list-model.js
@@ -1,29 +1,31 @@
-import { _original, initModel } from "./object-model.js"
+import { _original, MODE_CAST, initModel } from "./object-model.js"
 import { extend, has, isFunction, proxifyFn, proxifyModel, setConstructor } from "./helpers.js"
 
 export const initListModel = (base, constructor, def, init, clone, mutators, otherTraps = {}) => {
 
-	let model = function (list = model.default) {
+	let model = function (list = model.default, mode) {
 		list = init(list)
 
-		if (model.validate(list)) return proxifyModel(list, model, Object.assign({
-			get(l, key) {
-				if (key === _original) return l
+		if (mode === MODE_CAST || model.validate(list)) {
+			return proxifyModel(list, model, Object.assign({
+				get(l, key) {
+					if (key === _original) return l
 
-				let val = l[key];
-				return isFunction(val) ? proxifyFn(val, (fn, ctx, args) => {
-					if (has(mutators, key)) {
-						if (mutators[key]) args = mutators[key](args) // autocast method args
+					let val = l[key];
+					return isFunction(val) ? proxifyFn(val, (fn, ctx, args) => {
+						if (has(mutators, key)) {
+							if (mutators[key]) args = mutators[key](args) // autocast method args
 
-						let testingClone = clone(l)
-						fn.apply(testingClone, args)
-						model.validate(testingClone)
-					}
+							let testingClone = clone(l)
+							fn.apply(testingClone, args)
+							model.validate(testingClone)
+						}
 
-					return fn.apply(l, args)
-				}) : val
-			}
-		}, otherTraps))
+						return fn.apply(l, args)
+					}) : val
+				}
+			}, otherTraps))
+		}
 	}
 
 	extend(model, base)

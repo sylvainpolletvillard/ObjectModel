731439a8dd90c0a5c9ef4c7da3a67ea980e1516a
sylvainpolletvillard <sylvain.pollet.villard@gmail.com>
2018-11-09 11:33:33 +0100

    upgrade deps for patch 3.7.6

--- dist/object-model.js
+++ dist/object-model.js
@@ -1,4 +1,4 @@
-// ObjectModel v3.7.5 - http://objectmodel.js.org
+// ObjectModel v3.7.6 - http://objectmodel.js.org
 // MIT License - Sylvain Pollet-Villard
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
@@ -627,7 +627,7 @@
 
 	let controlMutation$1 = (model, array, key, value, applyMutation, canBeExtended) => {
 		let path = `Array[${key}]`;
-		let isInDef = (parseInt(key) >= 0 && (canBeExtended || key in array));
+		let isInDef = (+key >= 0 && (canBeExtended || key in array));
 		if (isInDef) value = checkDefinition(value, model.definition, path, model.errors, [], true);
 
 		let testArray = [...array];

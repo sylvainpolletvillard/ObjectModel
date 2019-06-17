471e834176c4fbdfe57e344f350960a2c59f363a
sylvainpolletvillard <sylvain.pollet.villard@gmail.com>
2019-06-17 12:49:52 +0200

    removed useless rollup name config

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,6 @@ const isProduction = process.env.BUILD === 'production';
 export default {
 	input: 'build/bundle-entry' + (isProduction ? '.js' : '.dev.js'),
 	output: {
-		name: 'window',
 		file: 'dist/object-model' + (isProduction ? '.min.js' : '.js'),
 		format: 'esm',
 		sourcemap: true,

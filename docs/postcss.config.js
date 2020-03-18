833d96146198fdef164a8cef766637c3bbc1e9c4
sylvainpolletvillard <sylvain.pollet.villard@gmail.com>
2020-03-18 20:12:26 +0100

    fix postcss-scrollbar on docs

--- docs/postcss.config.js
+++ docs/postcss.config.js
@@ -3,6 +3,7 @@ module.exports = {
 		require("postcss-import"),
 		require("postcss-simple-vars"),
 		require("postcss-nested"),
+		require("postcss-scrollbar"),
 		require("css-prefers-color-scheme/postcss"),
 		require("autoprefixer"),
 		require("postcss-normalize")({ forceImport: true }),

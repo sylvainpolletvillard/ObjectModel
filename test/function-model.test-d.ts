8278eb2c73977006cf66a3555c808618ff6faad9
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2022-10-23 16:29:27 +0200

    add more tests for function models

--- test/function-model.test-d.ts
+++ test/function-model.test-d.ts
@@ -1,5 +1,5 @@
 import {expectError, expectType} from 'tsd';
-import { FunctionModel } from '../types';
+import { FunctionModel, ObjectModel } from '../types';
 
 const F1 = FunctionModel(Number, String).return(Boolean);
 const f1 = F1((n,s) => +s === n);
@@ -10,3 +10,9 @@ expectType<boolean>(f1(1, "test"))
 expectError(f1("test", 2))
 
 expectError(f1(0)) 
+
+const OM = ObjectModel({ returnValue: Number })
+const F2 = FunctionModel().return(OM)
+const f2 = () => ({ returnValue: 1 })
+expectType<() => { returnValue: number}>(f2)
+expectType<number>(f2().returnValue)

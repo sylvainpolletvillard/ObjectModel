5099e8baad826eff77be0deb308fb8370238c62e
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2022-10-23 17:37:01 +0200

    prevent infinite type check recursion

--- types/definitions.d.ts
+++ types/definitions.d.ts
@@ -8,13 +8,15 @@ export type FromDefinition<T> = T extends StringConstructor | RegExp ? string
                               : T extends NumberConstructor ? number
                               : T extends BooleanConstructor ? boolean
                               : T extends ObjectModelDefinition ? FromObjectModelDefinition<T>
-                              : T extends ArrayModel<infer U> ? Array<FromDefinition<U>>
                               : T extends readonly [...infer U] ? FromUnionDefinition<U>
                               : T extends any[] ? FromDefinition<T[number]> // TypeScript can't infer array literals as tuples for now without <const> assertions, see https://github.com/microsoft/TypeScript/issues/16656
                               : T extends new () => infer ConstructorType ? ConstructorType
 					          : T
 
-export type FromObjectModelDefinition<D extends object> = { [K in keyof D]: FromDefinition<D[K]> }
+export type FromObjectPropertyDefinition<T> = T extends ArrayModel<infer U> ? FromDefinition<U>[]
+                                            : FromDefinition<T>
+
+export type FromObjectModelDefinition<D extends object> = { [K in keyof D]: FromObjectPropertyDefinition<D[K]> }
 
 export type FromUnionDefinition<T extends any[]> = T extends [infer X] ? Optional<X>
                                                  : FromDefinition<T[number]>

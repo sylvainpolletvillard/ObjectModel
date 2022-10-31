526edb61dacd15ef96ec76480363b46a3d03442d
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2022-10-31 11:56:20 +0100

    add more arraymodel tsd tests

--- test/array-model.test-d.ts
+++ test/array-model.test-d.ts
@@ -1,6 +1,6 @@
 import {expectType} from 'tsd';
 import { ArrayModel } from '../src/array-model';
-import { BasicModel, ObjectModel } from "../src/object-model"
+import { Model, BasicModel, ObjectModel } from "../src/object-model"
 
 expectType<number[]>(ArrayModel(Number)([1,2,3]))
 
@@ -16,3 +16,9 @@ expectType<(string | number)[]>(sizes)
 const Person = ObjectModel({ name: String, female: Boolean })
 const Group = ArrayModel(Person)
 expectType<{ name: string, female: boolean}[]>(Group([]))
+
+const ModelA = Model({ a: Number, c: Number })
+const ModelB = Model({ b: String, c: String })
+const AorBs = ArrayModel([ModelA, ModelB])
+const aorbs = AorBs([ { a: 1, c:0 }, { b: "2", c: ""}])
+expectType<({ a: number, c: number } | { b: string, c: string })[]>(aorbs)

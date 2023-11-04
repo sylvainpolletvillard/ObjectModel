9a3980b7b79c3795ea9d08c933415390e3958038
Ronald M Zielaznicki <git_6hiw@ronald.m.zielaznicki.com>
2023-11-04 01:32:04 -0400

    some adjustments

--- src/ref-model.js
+++ src/ref-model.js
@@ -1,13 +1,29 @@
 import { extend, symbolify } from './helpers.js';
-import { _check, _refs, formatDefinition, initModel } from './object-model.js';
+import { _check, _refs, cannot, formatDefinition, initModel } from './object-model.js';
 
 export default function RefModel(key) {
 	const symbol = symbolify(key);
-	return _refs.has(symbol) ? _refs.get(symbol) : initModel(undefined, RefModel);
+	let referencedModel;
+
+	return function(val, mode) {
+		if (referencedModel === undefined) {
+			referencedModel = _refs.get(symbol);
+		}
+
+		if (referencedModel && val !== undefined) {
+			return new referencedModel(val, mode);
+		}
+
+		return new initModel(symbol, RefModel);
+	}
 }
 
 extend(RefModel, Object, {
-	[_check]() {}, // _check is a noop for RefModel
+	[_check](val) {
+		if (val !== undefined) {
+			cannot(this, 'set value for ' + this.toString() + ' when model referenced is undefined')
+		}
+	},
 
 	toString(stack) {
 		return "Ref of " + formatDefinition(this.definition, stack)
